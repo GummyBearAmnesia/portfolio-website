ec4df572b401bbba81efb3b3af221982fb3ef2cc
Aamukta Thogata <aamukta.thogata@gmail.com>
2025-09-04 19:36:39 +0100

    Add skeleton setup for website

---
+++ my-portfolio/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import './header.css'
+
+export default function Header() {
+  return (
+    <nav className="Header">
+      <ul>
+        <li><a href="#Home">Home</a></li>
+        <li><a href="#About Me">About Me</a></li>
+        <li><a href="#Contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
---
+++ my-portfolio/src/components/Welcome.jsx
@@ -0,0 +1,16 @@
+import jiggly from '../assets/jiggly.jpg'
+import osha from '../assets/oshawott.webp'
+
+export default function Welcome() {
+  return (
+    <div className="Welcome">
+        <img
+            src={jiggly}
+            alt="There's meant to be an image here, sorry."S
+        />
+
+        <h1>Ammu's World</h1>
+        <p>Hi my name is Ammu, and I am like the best person in the whole world</p>
+    </div> 
+    );
+}
