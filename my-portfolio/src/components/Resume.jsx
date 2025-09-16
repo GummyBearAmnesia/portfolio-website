760af72fcd5e88c6b5c027d3c69b9ea10805f532
Aamukta Thogata <aamukta.thogata@gmail.com>
2025-09-16 03:21:27 +0100

    Add outline function for resume

---
+++ my-portfolio/src/components/Resume.jsx
@@ -0,0 +1,28 @@
+function Resume() {
+  return (
+
+    <div className="Resume">
+      <h1>Aamukta Thogata</h1>
+      <p>Email: aamukta.thogata@gmail.com | GitHub: github.com/gummybearamnesia</p>
+
+      <h3>Education</h3>
+      <p><strong>King’s College London</strong> – MSc Computer Science, 2023–2027</p>
+      <p>Student Rep | Expected 1st Class Honors</p>
+      
+      <p><strong>Woodford County High School</strong> - A*A*A Maths, Computer Science, Physics</p>
+      
+      <h3>Experience</h3>
+      <p><strong>Summer Intern, Vermont</strong> – June–Aug 2024</p>
+      <p>Worked on X, Y, Z…</p>
+
+      <h3>Skills</h3>
+      <p>Python, JavaScript, React, Vite, Unreal Engine, …</p>
+
+      <h3>Projects</h3>
+      <p><strong>Virtual Study Room</strong> – A project for …</p>
+    </div>
+      
+  )
+}
+
+export default Resume
--- my-portfolio/src/pages/Home.jsx
+++ my-portfolio/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Welcome from '../components/Welcome.jsx'
+import Resume from '../components/Resume.jsx'
 
 function Home() {
   return (
     <div className='Home'>
       {/* This is the welcome information */}
       <Welcome />
+      <Resume />
     </div>
   )
 }
