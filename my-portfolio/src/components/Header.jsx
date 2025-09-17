abb696af92c83621f1eb259c550bf700b972d118
Aamukta Thogata <aamukta.thogata@gmail.com>
2025-09-18 00:16:46 +0100

    Add animations

--- my-portfolio/src/components/Header.jsx
+++ my-portfolio/src/components/Header.jsx
@@ -6,8 +6,8 @@ export default function Header() {
     <nav className="Header">
       <ul>
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/resume">Resume</Link></li>
         <li><Link to="/projects">Projects</Link></li>
+        <li><Link to="/resume">Resume</Link></li>
         <li><Link to="/blog">Blog</Link></li>
         <li className="warcapes"><Link to="/warcapes">wARcAPES</Link></li>
       </ul>
