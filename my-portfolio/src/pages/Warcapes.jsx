0aca91d7180eeeb6011b73d79da0983a247f4e86
Aamukta Thogata <aamukta.thogata@gmail.com>
2025-09-18 00:24:09 +0100

    Add easter egg

--- my-portfolio/src/pages/Warcapes.jsx
+++ my-portfolio/src/pages/Warcapes.jsx
@@ -1,11 +1,33 @@
+import { useState } from 'react';
+import Confetti from 'react-confetti'; // npm install react-confetti
+import '../css/warcapes.css';
+
 function Warcapes() {
+  const [unlocked, setUnlocked] = useState(false);
+
   return (
     <div className='columns'>
+      {unlocked && <Confetti numberOfPieces={150} recycle={false} />}
+      
       <div className='column'>
-        <p> Not all heroes wear capes. </p>
+        <p
+          className={unlocked ? 'hero unlocked' : 'hero'}
+          onClick={() => setUnlocked(true)}
+        >
+          Not all heroes wear capes.
+        </p>
+
+        {unlocked && (
+          <div className="easter-egg">
+            🦸‍♂️🦸‍♀️✨ Loyal fans, please wait a bit longer. Pterri is on the way! ✨🦸‍♂️🦸‍♀️
+            <div className="floating-capes">
+              🦸‍♂️🦸‍♀️🦸‍♂️🦸‍♀️
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
 }
 
-export default Warcapes
+export default Warcapes;
