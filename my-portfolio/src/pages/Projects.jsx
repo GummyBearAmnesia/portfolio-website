5516e50f373074e3896e7486871cb806dae481ec
Aamukta Thogata <aamukta.thogata@gmail.com>
2025-09-17 23:45:37 +0100

    Seperate projects from resume section

--- my-portfolio/src/pages/Projects.jsx
+++ my-portfolio/src/pages/Projects.jsx
@@ -1,4 +1,5 @@
 import '../css/projects.css'
+import '../css/resume.css'
 import Robot from '../assets/projects/Robot.png'
 import Game from '../assets/projects/Game3.png'
 import Study from '../assets/projects/Study.png'
@@ -66,6 +67,53 @@ export default function Projects() {
             )}
           </div>
         ))}
+      </div>
+      
+      <div className="Resume">
+        <h3>Projects [in more detail]</h3>
+        <ul>
+        <li>
+          <strong>Hotel Delivery Robot – Python (ROS & YOLO) [Year 2]</strong>
+          <ul>
+            <li>Built an autonomous delivery robot with ROS, YOLO-based image detection, and actionlib navigation (pathfinding).</li>
+            <li>Designed SMACH state machines to handle guest requests and exceptions, planning robot actions in a dynamic environment.</li>
+            <li>Integrated computer vision, automation, and data-driven decision making for real-world service applications (such as in hotels).</li>
+          </ul>
+        </li>
+
+        <li>
+          <strong>Alternative Sudoku Solver – C++ [Year 2]</strong>
+          <ul>
+            <li>Created a heuristic Sudoku solver with a depth-first search algorithm and stack-based backtracking.</li>
+            <li>Applied optimisation and algorithmic analysis to evaluate constraint satisfaction problems efficiently.</li>
+          </ul>
+        </li>
+
+        <li>
+          <strong>Virtual Study Space Website – React, Django [Year 2]</strong>
+          <ul>
+            <li>Developed online study rooms with real-time chat and collaboration tools using WebSockets.</li>
+            <li>Used REST API to communicate with backend SQL database, collaborating via GitHub for version control and teamwork.</li>
+          </ul>
+        </li>
+
+        <li>
+          <strong>Monte Carlo Simulation – Python, MATLAB [Physics Project at University]</strong>
+          <ul>
+            <li>Implemented Monte Carlo simulations to model success probabilities of contestants crossing a probabilistic “bridge” (inspired by Squid Game).</li>
+            <li>Designed statistical experiments across 10,000+ trials, analysing distributions of outcomes and computing success probabilities.</li>
+            <li>Applied randomised algorithms, probability, and data-driven reasoning to evaluate decision-making under uncertainty.</li>
+          </ul>
+        </li>
+
+        <li>
+          <strong>2D RPG Game – Unity and C# [A-Level Final Project]</strong>
+          <ul>
+            <li>Implemented core gameplay mechanics (player movement, interactions, event systems) using Unity and C#.</li>
+            <li>Created original pixel art and animation rigs in Photoshop and After Effects, integrating them into the engine for responsive character animations.</li>
+          </ul>
+        </li>
+      </ul>
       </div>
     </div>
   )
