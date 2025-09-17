3fbc5439a778d99759e4e6b3d9d4a358be5b0339
Aamukta Thogata <aamukta.thogata@gmail.com>
2025-09-18 00:33:10 +0100

    Add blogs layout

--- my-portfolio/src/pages/Blog.jsx
+++ my-portfolio/src/pages/Blog.jsx
@@ -1,11 +1,30 @@
-function Blog() {
-  return (
-    <div className='columns'>
-      <div className='column'>
-        <p> Eventually there will be a blog here! Still working on it</p>
-      </div>
-    </div>
-  )
-}
+import React from 'react';
+import '../css/blog.css';
 
-export default Blog
+const blogPosts = [
+  {
+    title: "My First Blog Post",
+    image: "/path/to/image1.jpg",
+    excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+    link: "/blog/post-1"
+  },
+];
+
+export default function Blog() {
+  return (
+    <div className="Blog">
+      {blogPosts.map((post, index) => (
+        <div key={index} className="blog-card">
+          <div className="blog-image">
+            <img src={post.image} alt={post.title} />
+          </div>
+          <div className="blog-text">
+            <h3>{post.title}</h3>
+            <p>{post.excerpt}</p>
+            <a href={post.link} className="read-more">Read More</a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
