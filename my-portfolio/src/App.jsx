fcb6aab88857fc73a25aac693107488aed13e8a1
Aamukta Thogata <aamukta.thogata@gmail.com>
2025-10-10 21:58:09 +0100

    Initial blog setup

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Main pages and components
 import Header from './components/Header.jsx';
 import Home from './pages/Home.jsx';
 import Projects from './pages/Projects.jsx';
@@ -6,6 +8,11 @@ import Resume from './pages/Resume.jsx'
 import Blog from './pages/Blog.jsx';
 import Warcapes from './pages/Warcapes.jsx';
 import NotFound from './pages/NotFound.jsx';
+
+// Many Blog Posts
+import Blog1 from './assets/blogs/post-1.jsx';
+
+// css files
 import './App.css'
 
 function App() {
@@ -19,6 +26,9 @@ function App() {
         <Route path="/projects" element={<Projects />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/warcapes" element={<Warcapes />} />
+
+        {/* many blog pages */}
+        <Route path="/blog/post-1" element={<Blog1 />} />
 
         {/* Catch-all 404 */}
         <Route path="*" element={<NotFound />} />
---
+++ my-portfolio/src/assets/blogs/post-1.jsx
@@ -0,0 +1,11 @@
+export default function Blog1() {
+  return (
+    <div className="Blog1">
+        <div className="columns">
+            <div className="column">
+                <p> Insert Great Content </p>
+            </div>
+        </div>
+    </div> 
+    );
+}
